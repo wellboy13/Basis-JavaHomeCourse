ad1be052c669694bc4f188e75685555f2fbc9885
wellboy13 <artzemchenko@gmail.com>
2023-09-15 09:26:13 +0200

    Add reversedText

--- src/main/java/de/telran/reversedText/ReverseMethodText.java
+++ src/main/java/de/telran/reversedText/ReverseMethodText.java
@@ -8,7 +8,7 @@ public class ReverseMethodText {
         StringBuilder reversed = new StringBuilder(length); // создаем экземпляр класса StringBuilder
 
         for (int i = length - 1; i >= 0; i--) {
-            reversed.append(text.charAt(i)); //запускаем цикл для обратной печати
+            reversed.append(text.charAt(i)); //запускаем цикл для обратной печати с большего значения к меньшему
         }
 
         return reversed.toString(); //возвращаем текст
